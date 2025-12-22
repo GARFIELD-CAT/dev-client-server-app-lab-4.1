676ffef86cf140b7ee0f9622da116bce429a1e99
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-22 21:48:04 +0300

    add init producer service

---
+++ src/main/java/ru/rksp/yagunov_producer/YagunovProducerApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.yagunov_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagunovProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagunovProducerApplication.class, args);
+        System.out.println("Ok");
+	}
+}
---
+++ src/test/java/ru/rksp/yagunov_producer/YagunovProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.yagunov_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagunovProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
