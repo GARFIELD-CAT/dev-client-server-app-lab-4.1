20c466645de7f1e2e5f743705d892fc3f36723a2
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-25 17:22:26 +0300

    add logs

--- src/main/java/ru/rksp/yagunov_producer/controller/StudentProducerController.java
+++ src/main/java/ru/rksp/yagunov_producer/controller/StudentProducerController.java
@@ -1,6 +1,7 @@
 package ru.rksp.yagunov_producer.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import ru.rksp.yagunov_producer.model.StudentMessage;
 
 import java.time.Instant;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/producer/students")
 @RequiredArgsConstructor
@@ -28,10 +30,12 @@ public class StudentProducerController {
 
     @PostMapping
     public ResponseEntity<String> sendStudent(@RequestBody StudentMessage request) {
+        log.info("------------Запрос успешно получен------------");
         // Добавим timestamp на стороне продьюсера
         request.setCreatedAt(Instant.now());
 
         rabbitTemplate.convertAndSend(exchangeName, routingKey, request);
+        log.info("------------Сообщение успешно добавлено в очередь------------");
 
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Message sent to RabbitMQ");
     }
